58802511669e52775a967b7ade30ed5c0effb870
Justin Buckley <justin.buckley@apexmediagrp.com>
2018-05-02 21:13:28 -0700

    Initial commit with required paths files and folders
    @jmbuckley

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger) VALUES ('Triple Bacon Cheeseburger');
+INSERT INTO burgers (burger) VALUES ('Animal Style Cheeseburger');
+INSERT INTO burgers (burger) VALUES ('Jalapeno Cheddar Burger');
+INSERT INTO burgers (burger) VALUES ('Tofu Burger');
+INSERT INTO burgers (burger) VALUES ('Regular Ole Boring Burger');
+INSERT INTO burgers (burger) VALUES ('Turk-a-Lurk Burger);
